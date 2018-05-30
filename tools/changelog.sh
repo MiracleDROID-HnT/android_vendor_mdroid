ffd22e9c5a513a70402cee5e4817ff56b34171c0
Lup Gabriel <gwolf2u.website@gmail.com>
2018-05-30 22:37:21 +0300

    make it variable based
    define changelog_days using 'export changelog_days=10'
    this can be done before intiate build environment (. build/envsetup.sh)

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -16,10 +16,19 @@ touch $Changelog
 # Print something to build output
 echo ${bldppl}"Generating changelog..."${txtrst}
 
-echo "For how many days do you want to generate changelog? (🕑 timeout 15 seconds - default to 7 days)"
-read -r -t 15 days || days=7
+# define changelog_days using 'export changelog_days=10'
+# this can be done before intiate build environment (. build/envsetup.sh)
+if [ -z $changelog_days ];then
+	changelog_days=10
+else
+	if [ $changelog_days > 30 ]; then
+        echo "Changelog can not generated for more then 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
+        read -r -t 15 changelog_days || changelog_days=10
+	fi
+fi
+echo $changelog_days
 
-for i in $(seq $days);
+for i in $(seq $changelog_days);
 do
 export After_Date=`date --date="$i days ago" +%F`
 k=$(expr $i - 1)
