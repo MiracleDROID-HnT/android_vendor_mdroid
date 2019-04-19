ee259ee4a1abc3fea59814721ca403de8ae75e97
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-04-19 10:17:34 +0300

    Add Circle QS Tile style [4/4]

--- config/common.mk
+++ config/common.mk
@@ -218,6 +218,7 @@ PRODUCT_PACKAGES += \
     MDroidTealAccent \
     MDroidWhiteAccent \
     MDroidYellowAccent \
+    QStileCircle \
     QStileCircleTrim \
     QStileDefault \
     QStileDualToneCircle \
