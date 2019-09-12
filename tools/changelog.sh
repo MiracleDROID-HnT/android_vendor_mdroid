0f2c40ad3a4d1a81253f49b3e11d235410c9d645
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2019-09-12 21:42:49 +0300

    Fix changelog script

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -21,7 +21,7 @@ echo ${bldppl}"Generating changelog..."${txtrst}
 if [ -z $changelog_days ];then
 	changelog_days=10
 else
-	if [ $changelog_days > 30 ]; then
+	if [[ $changelog_days > 30 ]]; then
         echo "Changelog can not generated for more then 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
         read -r -t 15 changelog_days || changelog_days=10
 	fi
